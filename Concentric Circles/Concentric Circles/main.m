ab6d245a9b8fdc211c8118ae53cca71006dc0d7b
Vinay Jain <vinay.jn7@gmail.com>
2014-11-27 01:55:27 +0530

    Initialised project Concentric Circle

---
+++ Concentric Circles/Concentric Circles/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Concentric Circles
+//
+//  Created by Vinay Jain on 27/11/14.
+//  Copyright (c) 2014 Vinay Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Concentric Circles/Concentric CirclesTests/Concentric_CirclesTests.m
@@ -0,0 +1,40 @@
+//
+//  Concentric_CirclesTests.m
+//  Concentric CirclesTests
+//
+//  Created by Vinay Jain on 27/11/14.
+//  Copyright (c) 2014 Vinay Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Concentric_CirclesTests : XCTestCase
+
+@end
+
+@implementation Concentric_CirclesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
