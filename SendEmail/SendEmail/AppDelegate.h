46466ec074ea6f2f24c6037225d7dc960f879be6
Vinay Jain <vinay.jn7@gmail.com>
2014-12-16 00:19:48 +0530

    Added SendEmail Project

---
+++ SendEmail/SendEmail/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SendEmail
+//
+//  Created by Vinay on 12/16/14.
+//  Copyright (c) 2014 Vinay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SendEmail/SendEmailTests/SendEmailTests.m
@@ -0,0 +1,40 @@
+//
+//  SendEmailTests.m
+//  SendEmailTests
+//
+//  Created by Vinay on 12/16/14.
+//  Copyright (c) 2014 Vinay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SendEmailTests : XCTestCase
+
+@end
+
+@implementation SendEmailTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
