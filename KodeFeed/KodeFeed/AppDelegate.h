2ef9c10d9271497de8217bb78a286dd815351bfc
Vinay Jain <vinay.jn7@gmail.com>
2014-12-15 18:43:34 +0530

    added KodeFeed, a basic app for demonstration of REST based web service access

---
+++ KodeFeed/KodeFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KodeFeed
+//
+//  Created by Vinay on 12/11/14.
+//  Copyright (c) 2014 Vinay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KodeFeed/KodeFeed/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  KodeFeed
+//
+//  Created by Vinay on 12/11/14.
+//  Copyright (c) 2014 Vinay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
