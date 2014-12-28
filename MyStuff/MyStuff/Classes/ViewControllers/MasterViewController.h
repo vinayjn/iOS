4ccc820be2bfdba4eb5602a13866879a37fd2b06
Vinay Jain <vinay.jn7@gmail.com>
2014-12-29 00:00:04 +0530

    Updated Folder Structure

---
+++ MyStuff/MyStuff/Classes/ViewControllers/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  MyStuff
+//
+//  Created by Vinay on 12/25/14.
+//  Copyright (c) 2014 Vinay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
