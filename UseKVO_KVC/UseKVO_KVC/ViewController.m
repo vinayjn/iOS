a575e9bd36bcd2221c64d2a048e336b279437a15
Vinay Jain <vinay.jn7@gmail.com>
2015-03-19 01:08:32 +0530

    Use context to uniquely observe values of same type of objects

--- UseKVO_KVC/UseKVO_KVC/ViewController.m
+++ UseKVO_KVC/UseKVO_KVC/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+static void *child1Context = &child1Context;
+static void *child2Context = &child2Context;
 
 @interface ViewController ()
 
@@ -14,7 +16,6 @@
 
 @property (nonatomic, strong) Children *child2;
 
-@property (nonatomic, strong) Children *child3;
 
 @end
 
@@ -25,10 +26,10 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.child1 = [[Children alloc] init];
-//    self.child1.name = @"George";
-//    self.child1.age = 15;
-//    
-//    NSLog(@"%@, %d", self.child1.name, self.child1.age);
+    //    self.child1.name = @"George";
+    //    self.child1.age = 15;
+    //
+    //    NSLog(@"%@, %d", self.child1.name, self.child1.age);
     
     [self.child1 setValue:@"George" forKey:@"name"];
     [self.child1 setValue:[NSNumber numberWithInteger:15] forKey:@"age"];
@@ -38,31 +39,59 @@
     
     NSLog(@"%@, %d", childName, childAge);
     
+    
+    self.child2 = [[Children alloc] init];
+    
+    childName = [self.child2 valueForKey:@"name"];
+    childAge = [[self.child2 valueForKey:@"age"] integerValue];
+    
 }
 - (IBAction)changedValue:(UIButton *)sender {
     
     [self.child1 setValue:@"Vinay" forKey:@"name"];
     [self.child1 setValue:[NSNumber numberWithInteger:22] forKey:@"age"];
+    
+    [self.child2 setValue:@"Jain" forKey:@"name"];
+    [self.child2 setValue:[NSNumber numberWithInteger:22] forKey:@"age"];
 }
 
 -(void)viewWillAppear:(BOOL)animated{
-    [self.child1 addObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
-    [self.child1 addObserver:self forKeyPath:@"age" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
+    [self.child1 addObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:child1Context];
+    [self.child1 addObserver:self forKeyPath:@"age" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:child1Context];
+    
+    [self.child2 addObserver:self forKeyPath:@"name" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:child2Context];
+    [self.child2 addObserver:self forKeyPath:@"age" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:child2Context];
     
 }
 
 -(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context{
     
-    if ([keyPath isEqualToString:@"name"]) {
-        NSLog(@"The name of the child was changed.");
-        NSLog(@"%@", change);
+    if (context == child1Context) {
+        
+        
+        if ([keyPath isEqualToString:@"name"]) {
+            NSLog(@"The name of the first child was changed.");
+            NSLog(@"%@", change);
+        }
+        
+        if ([keyPath isEqualToString:@"age"]) {
+            NSLog(@"The age of the first child was changed.");
+            NSLog(@"%@", change);
+        }
     }
-    
-    if ([keyPath isEqualToString:@"age"]) {
-        NSLog(@"The age of the child was changed.");
-        NSLog(@"%@", change);
+    else if (context == child2Context) {
+        
+        
+        if ([keyPath isEqualToString:@"name"]) {
+            NSLog(@"The name of the second child was changed.");
+            NSLog(@"%@", change);
+        }
+        
+        if ([keyPath isEqualToString:@"age"]) {
+            NSLog(@"The age of the second child was changed.");
+            NSLog(@"%@", change);
+        }
     }
-    
 }
 
 - (void)didReceiveMemoryWarning {
