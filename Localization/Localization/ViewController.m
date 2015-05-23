503b63f35cdca7e5f0e442ff5404e8856496cbb7
Vinay Jain <vinay.jn7@gmail.com>
2015-05-23 10:23:41 +0530

    Added Localizations to test Xcode 6 features

---
+++ Localization/Localization/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Localization
+//
+//  Created by Vinay Jain on 5/23/15.
+//  Copyright (c) 2015 Vinay Jain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
